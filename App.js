ff3e57199a5ddfdc9787443fda5f68a0b118dda8
jerodgers1 <james.rodgers0047@gmail.com>
2017-12-11 21:21:25 -0600

    Uses a third party modules (lodash) to determain is a variable is a string and to remove duplicate variables from a string.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 //Loads Libraries
 
-//Loads File
-const Notes = require('./Notes.js');
+//third party modules
+const lodash = require('lodash');
 
-var response = Notes.addNote();
-console.log(response);
+console.log(`lodash returns true if a boolean variable is a string other wise it returns false. \nResult is ${lodash.isString(true)}`);
 
-var product = Notes.add(2,3);
-console.log(`2 plue 3 equals ${product}.`);
+console.log(`lodash returns true if "hello" is a string other wise it returns false. \nResult is ${lodash.isString("hello")}`);
+
+var filteredArray = lodash.uniq(['James', 1, 'James', 1, 2, 3, 3, 4])
+console.log(`this array ['James', 1, 'James', 1, 2, 3, 3, 4] is filtered using lodash to remove duplicate variables. \nSo the new array is [${filteredArray}]`);
